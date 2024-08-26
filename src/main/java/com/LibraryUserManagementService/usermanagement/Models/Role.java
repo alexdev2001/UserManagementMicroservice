b76e0416d3f8501e34fc5621c8e67c5c7cfbf5fb
alexdev2001 <aleximani201@gmail.com>
2024-08-26 14:48:30 +0200

    first commit on user microservice

---
+++ src/main/java/com/LibraryUserManagementService/usermanagement/Models/Role.java
@@ -0,0 +1,27 @@
+package com.LibraryUserManagementService.usermanagement.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(
+        name = "role"
+)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleID;
+    private String roleName;
+
+    @ManyToMany(mappedBy = "role")
+    private Set<User> users;
+}
---
+++ src/main/java/com/LibraryUserManagementService/usermanagement/Models/User.java
@@ -0,0 +1,35 @@
+package com.LibraryUserManagementService.usermanagement.Models;
+
+import jakarta.persistence.*;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.util.Set;
+
+@Entity
+@Table(
+        name = "users"
+)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userID;
+    private String username;
+    private String password;
+    private String membership_date;
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> role;
+}
